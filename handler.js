f65e31f5fc532b3594993384ca1e1ddca0f78475
Oscar Montoya <oscardavid.montoyagomez@naranja.com>
2022-11-23 22:56:10 -0500

    update

--- handler.js
+++ handler.js
@@ -1,47 +1,41 @@
 'use strict';
 
-// const { DynamoDB } = require("aws-sdk")
 const DynamoDB = require('aws-sdk/clients/dynamodb');
 
-const db = new DynamoDB.DocumentClient()
-const TableName = process.env.TABLE_NAME
-
+const db = new DynamoDB.DocumentClient();
+const TableName = process.env.TABLE_NAME;
 
 module.exports.create = async (event) => {
-
   event.body = JSON.parse(event.body);
 
   const newAnimal = {
     name: event.body.name,
     age: event.body.age,
     family: event.body.family,
-  }
+  };
 
   await db
     .put({
       TableName,
       Item: newAnimal,
     })
-    .promise()
+    .promise();
 
-  return { statusCode: 200, body: JSON.stringify(newAnimal) }
-}
-
+  return { statusCode: 200, body: JSON.stringify(newAnimal) };
+};
 
 module.exports.list = async (event) => {
   const animals = await db
     .scan({
       TableName,
     })
-    .promise()
+    .promise();
 
-  return { statusCode: 200, body: JSON.stringify(animals) }
-}
-
-
+  return { statusCode: 200, body: JSON.stringify(animals) };
+};
 
 module.exports.delete = async (event) => {
-  const animalToBeRemovedName = event.pathParameters.name
+  const animalToBeRemovedName = event.pathParameters.name;
   console.log(animalToBeRemovedName);
   await db
     .delete({
@@ -50,8 +44,7 @@ module.exports.delete = async (event) => {
         name: animalToBeRemovedName,
       },
     })
-    .promise()
+    .promise();
 
-  return { statusCode: 200 }
-}
-
+  return { statusCode: 200 };
+};
